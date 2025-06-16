b47968988637971b99b7cd11a75048bcf008341d
7eXx <marco.tex@live.com>
2025-06-16 22:05:35 +0200

    fix mqtt server

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
 // MQTT broker info
-const char* mqtt_server = "192.168.1.95"; // You can use test.mosquitto.org, etc.
+const char* mqtt_server = "mosquitto.raspi-home"; // You can use test.mosquitto.org, etc.
 const int mqtt_port = 1883;
